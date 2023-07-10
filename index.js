5abe8bed1731ace1c8e6fcfa01823b4c4e96df85
Aditya nur Iskandar <adityanuriskandar@mahasiswa.unikom.ac.id>
2023-07-10 21:34:58 +0700

    fix

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-import cors from 'cors'
-import express from 'express'
-import dotenv from 'dotenv'
-import ProductsRoute from "./routes/ProductsRoute.js"
-import UserRoute from "./routes/UserRoute.js"
-import cookieParser from 'cookie-parser'
-dotenv.config()
+import cors from "cors";
+import express from "express";
+import dotenv from "dotenv";
+import ProductsRoute from "./routes/ProductsRoute.js";
+import CategoryRoute from "./routes/CategoryRoute.js";
+import UserRoute from "./routes/UserRoute.js";
+import cookieParser from "cookie-parser";
+dotenv.config();
 
 dotenv.config();
 
 const port = process.env.APP_PORT;
 const app = express();
 
-app.use(cors({credentials:true, origin:'http://localhost:3000'}))
-app.use(cookieParser())
-app.use(express.json())
-app.use(ProductsRoute)
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cookieParser());
+app.use(express.json());
+app.use(ProductsRoute);
 app.use(CategoryRoute);
-app.use('/api/v1', UserRoute);
+app.use("/api/v1", UserRoute);
 
-app.get('/', (req, res) => {
-  res.send('e-textile api server')
-})
+app.get("/", (req, res) => {
+  res.send("e-textile api server");
+});
 
 app.listen(port, () => {
   console.log(`server running on ${port}`);
---
+++ prisma/migrations/20230710140800_add_user/migration.sql
@@ -0,0 +1,31 @@
+-- AlterTable
+ALTER TABLE `products` ADD COLUMN `categoryId` INTEGER NULL,
+    ADD COLUMN `desc` VARCHAR(191) NULL;
+
+-- AlterTable
+ALTER TABLE `users` MODIFY `role` ENUM('USER', 'ADMIN') NULL DEFAULT 'USER',
+    MODIFY `refresh_token` VARCHAR(191) NULL;
+
+-- CreateTable
+CREATE TABLE `Category` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `icon` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Image` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `productId` INTEGER NOT NULL,
+    `imageUrl` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Products` ADD CONSTRAINT `Products_categoryId_fkey` FOREIGN KEY (`categoryId`) REFERENCES `Category`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Image` ADD CONSTRAINT `Image_productId_fkey` FOREIGN KEY (`productId`) REFERENCES `Products`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
